d9cb7a3d6843cb7124f516ccf124e2fe7458a97e
Hirusha Fernando <heshanhfernando@gmail.com>
2024-03-19 09:00:17 +0530

    Initial Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using AuditTrialTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuditTrialTest.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<Book> Books { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.Property(e => e.Id).IsRequired().UseIdentityColumn();
+                entity.Property(e => e.Title).IsRequired();
+                entity.Property(e => e.Description).IsRequired();
+                entity.Property(e => e.Author).IsRequired();
+            });
+        }
+    }
+}
---
+++ Data/Services/BookService.cs
@@ -0,0 +1,48 @@
+﻿using AuditTrialTest.Models;
+using AuditTrialTest.Pages;
+
+namespace AuditTrialTest.Data.Services
+{
+    public class BookService
+    {
+        private readonly ApplicationDbContext _context;
+        public BookService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddBook(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool RemoveBook(Book book)
+        {
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool UpdateBook(Book book) 
+        { 
+            _context.Books.Update(book);
+            _context.SaveChanges();
+            return true;
+        }
+
+         public Book? GetBook(int id) 
+        { 
+            var book = _context.Books.FirstOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            var books = new List<Book>();
+            books = _context.Books.ToList();
+            return books;
+        }
+    }
+}
